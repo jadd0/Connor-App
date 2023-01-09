77bcbdcb05b67c3d3f0ac94f767b7860d64d97ec
jadd0 <102069567+jadd0@users.noreply.github.com>
2023-01-09 22:13:52 +0000

    testing for job class

--- src/routes/api/new/+server.ts
+++ src/routes/api/new/+server.ts
@@ -1,15 +1,17 @@
-import { circlesClass } from '../../../stores'
+import { job } from '../../../stores'
+import { db } from '../../../stores';
 import { get } from 'svelte/store';
 
-const Circles = get(circlesClass);
+
+const Job = get(job);
+const DB = get(db);
 
 
 export const GET: any = async ({ request }) => {
 	
-  await Circles.createCircle({ 
-    username: 'jadd',
-    name: 'Jadd\'s Circle',
-   })
-
+  const user = await DB.getValue({ table: 'Users', value: { username: 'jadd' } })
+  const job = await DB.getValue({ table: 'Jobs', value: { uuid: '553b9ef4-809e-4c04-85a7-b03e63ebf435' }})
+  console.log({job})
+  await Job.completeJob({ jobID: job.uuid, user: user.uuid, amountPaid: 12 })
 	return new Response('hello');
 }
