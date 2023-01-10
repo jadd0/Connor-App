d61b238111fc943d397cb4ab06efb603612e67dc
jadd0 <102069567+jadd0@users.noreply.github.com>
2023-01-10 17:25:40 +0000

    activeJob endpoint complete (untested)

---
+++ src/routes/api/job/activeJob/+server.ts
@@ -0,0 +1,53 @@
+import { error, redirect } from '@sveltejs/kit';
+import { job, auth, db } from '../../../../stores';
+import { get } from 'svelte/store';
+
+const Job = get(job);
+const Auth = get(auth);
+const DB = get(db);
+
+async function checkNotActive(jobID: string) {
+  const job = await DB.getValue({ table: 'ActiveJobs', value: { jobID } })
+  
+  if (!job) return true
+
+  return false
+}
+
+
+/** @type {import('./$types').Load} */
+export const POST: any = async ({ request }) => {
+	const cookie = Auth.Parse.parseCookie(request.headers.get('cookie'));
+	if (cookie.key == undefined) {
+		throw error(401, 'Not authorised');
+	}
+
+	const auth = await Auth.checkKey(cookie.key);
+	if (!auth) {
+		throw error(401, 'Not authorised');
+	}
+
+	const { jobID, toBeCompletedDate } = await request.json();
+	if (jobID == undefined || toBeCompletedDate == undefined) {
+		throw error(406, 'Missing either job ID or completion');
+	}
+
+  if (!await checkNotActive(jobID)) {
+    throw error(401, 'You are unauthorised to mark this job as completed');
+  }
+
+	const res = await Job.activeJob({
+		jobID,
+		user: auth.uuid,
+    toBeCompletedDate
+	});
+
+	if (!res) {
+		throw error(
+			500,
+			'Sorry, something has gone wrong on our end. If this continues to occur, please do not hesitate to email me at jaddalkwork@gmail.com'
+		);
+	}
+
+	return new Response('Job created successfully.');
+};
