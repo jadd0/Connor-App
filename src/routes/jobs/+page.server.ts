e0ee0b3f19bc08dcc48bbb77551e7a3a1f402928
jadd0 <102069567+jadd0@users.noreply.github.com>
2023-01-11 21:19:23 +0000

    job route started and experimentally working

---
+++ src/routes/jobs/+page.server.ts
@@ -0,0 +1,25 @@
+import { error, redirect } from "@sveltejs/kit";
+import { db, auth } from '../../stores'
+import { get } from 'svelte/store';
+
+const DB = get(db)
+const Auth = get(auth)
+
+/** @type {import('./$types').Load} */
+export const load: any = async ({ request }) => {
+  const cookie = Auth.Parse.parseCookie(request.headers.get("cookie"));
+  console.log(cookie)
+	if (cookie.key == undefined) {
+		throw redirect(307, "/login");
+	}
+
+	const auth = await Auth.checkKey(cookie.key)
+  console.log(auth)
+	if (!auth) {
+		throw redirect(307, "/login");
+	}
+
+	return {
+		username: auth
+	}
+}
---
+++ src/routes/jobs/+page.svelte
@@ -0,0 +1,90 @@
+<script>
+  import Nav from '../__components/__nav/+page.svelte'
+</script>
+
+<body>
+  <Nav />
+
+  <div class="holder">
+    <h1>What are you looking to do?</h1>
+    <div class="buttonHolder">
+      <a class="choice" href='/jobs/find'><span class="big">FIND</span><span class="small">A JOB</span></a>
+      <a class="choice" href='/jobs/post'><span class="big">POST</span><span class="small">A JOB</span></a>
+    </div>
+  </div>
+</body>
+
+<style>
+  @import '/styles.css';
+  
+  body {
+    width: calc(100vw - 70px);
+    float: right;
+  }
+
+  .holder {
+    width: 100%;
+    height: 100%;
+
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .holder > h1 {
+    margin-top: -100px;
+  }
+
+  .holder > .buttonHolder {
+    width: 90%;
+    height: 30%;
+
+    margin: 0 auto;
+    margin-top: 35px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+  }
+
+  .holder > .buttonHolder > .choice {
+    width: 250px;
+    height: 150px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    background: #262626;
+    border-radius: 10px;
+
+    transition: all 0.3s ease-in-out; 
+    cursor: pointer;
+  }
+
+  .holder > .buttonHolder > .choice:hover {
+    transform: scale(1.05);
+    background: #2e2e2e;
+  }
+
+  .holder > .buttonHolder > .choice > .big {
+    font-size: 40px;
+    font-family: BoldCircular;
+  }
+
+  .holder > .buttonHolder > .choice > .small {
+    font-size: 20px;
+  }
+
+
+
+  h1 {
+    color: white;
+
+  }
+</style>
