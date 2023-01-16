99229cc0387190a9cd1f4b0046c168ded800af45
jadd0 <102069567+jadd0@users.noreply.github.com>
2023-01-16 12:48:36 +0000

    changed /jobs to now use components

--- src/routes/jobs/+page.svelte
+++ src/routes/jobs/+page.svelte
@@ -1,98 +1,108 @@
-<script lang='ts'>
-  import Nav from '../__components/__nav/+page.svelte'
+<script lang="ts">
+	import Nav from '../__components/__nav/+page.svelte';
 
-  export let data: {
-		username: string,
-		name: string,
-		uuid: string,
-		role: string
+	import Admin from './__components/admin/+page.svelte';
+	import Writer from './__components/writer/+page.svelte';
+	import Client from './__components/client/+page.svelte';
+
+	export let data: {
+		username: string;
+		name: string;
+		uuid: string;
+		role: string;
 	};
-  
+
+	console.log(data);
 </script>
 
 <body>
-  <Nav data={data}/>
+	<Nav {data} />
 
-  <div class="holder">
+	{#if data.role == 'Admin'}
+		<Admin {data} />
+	{:else if data.role == 'Writer'}
+		<Writer {data} />
+	{:else}
+		<Client {data} />
+	{/if}
+
+	<!-- <div class="holder">
     <h1>What are you looking to do?</h1>
     <div class="buttonHolder">
       <a class="choice" href='/jobs/find'><span class="big">FIND</span><span class="small">A JOB</span></a>
       <a class="choice" href='/jobs/post'><span class="big">POST</span><span class="small">A JOB</span></a>
     </div>
-  </div>
+  </div> -->
 </body>
 
 <style>
-  @import '/styles.css';
-  
-  body {
-    width: calc(100vw - 70px);
-    float: right;
-  }
+	@import '/styles.css';
 
-  .holder {
-    width: 100%;
-    height: 100%;
+	body {
+		width: calc(100vw - 70px);
+		float: right;
+	}
 
-    margin: 0 auto;
+	.holder {
+		width: 100%;
+		height: 100%;
 
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
+		margin: 0 auto;
 
-  .holder > h1 {
-    margin-top: -100px;
-  }
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
 
-  .holder > .buttonHolder {
-    width: 90%;
-    height: 30%;
+	.holder > h1 {
+		margin-top: -100px;
+	}
 
-    margin: 0 auto;
-    margin-top: 35px;
+	.holder > .buttonHolder {
+		width: 90%;
+		height: 30%;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 20px;
-  }
+		margin: 0 auto;
+		margin-top: 35px;
 
-  .holder > .buttonHolder > .choice {
-    width: 250px;
-    height: 150px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 20px;
+	}
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
+	.holder > .buttonHolder > .choice {
+		width: 250px;
+		height: 150px;
 
-    background: #262626;
-    border-radius: 10px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
 
-    transition: all 0.3s ease-in-out; 
-    cursor: pointer;
-  }
+		background: #262626;
+		border-radius: 10px;
 
-  .holder > .buttonHolder > .choice:hover {
-    transform: scale(1.05);
-    background: #2e2e2e;
-  }
+		transition: all 0.3s ease-in-out;
+		cursor: pointer;
+	}
 
-  .holder > .buttonHolder > .choice > .big {
-    font-size: 40px;
-    font-family: BoldCircular;
-  }
+	.holder > .buttonHolder > .choice:hover {
+		transform: scale(1.05);
+		background: #2e2e2e;
+	}
 
-  .holder > .buttonHolder > .choice > .small {
-    font-size: 20px;
-  }
+	.holder > .buttonHolder > .choice > .big {
+		font-size: 40px;
+		font-family: BoldCircular;
+	}
 
+	.holder > .buttonHolder > .choice > .small {
+		font-size: 20px;
+	}
 
-
-  h1 {
-    color: white;
-
-  }
+	h1 {
+		color: white;
+	}
 </style>
