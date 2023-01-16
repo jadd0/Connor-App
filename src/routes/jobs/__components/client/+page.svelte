15fea26c9497b68c3e26b94d53f385751494fade
jadd0 <102069567+jadd0@users.noreply.github.com>
2023-01-16 12:48:53 +0000

    /jobs components

---
+++ src/routes/jobs/__components/client/+page.svelte
@@ -0,0 +1,63 @@
+<script lang='ts'>
+  import Input from '../../../__components/__input/+page.svelte'
+
+  export let data: {
+		username: string,
+		name: string,
+		uuid: string,
+		role: string
+	};
+
+  let title: string = ''
+  let description: string = '';
+  let price: number = 0;
+  let type: string = ''
+
+  function err() {
+
+  }
+
+  function success() {
+
+  }
+
+  async function postJob() {
+    const res = await fetch("/api/job/newJob", {
+			method: "post",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				title,
+        description,
+        price,
+        type
+			}),
+		});
+
+    if (!res.ok) {
+      err()
+      return
+    }
+
+    success()    
+  }
+</script>
+
+  <div class="holder">
+    <h1>Post a job</h1>
+    <div class="formHolder">
+      <Input prop='Title' bind:data={title}/>
+      <Input prop='Description' bind:data={description}/>
+      <Input prop='Price' bind:data={price}/>
+      <Input prop='Type' bind:data={type}/>
+      <button class="loginButton" on:click={postJob}>Post</button>
+    </div>
+  </div>
+
+
+
+<style>
+  @import '/styles.css';
+</style>
---
+++ src/routes/jobs/__components/writer/+page.svelte
@@ -0,0 +1,63 @@
+<script lang='ts'>
+  import Input from '../../../__components/__input/+page.svelte'
+
+  export let data: {
+		username: string,
+		name: string,
+		uuid: string,
+		role: string
+	};
+
+  let jobs: any = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+  let colours: string[] = ['red', 'green', 'yellow', 'blue', 'orange', 'purple', 'pink', 'black', 'light gray']
+ 
+</script>
+
+
+  <div class="holder">
+    <div id="filters"></div>
+    <div id="content">
+      {#each jobs as job}
+        <div class="job">
+          <div class="colourHolder" style='background: {colours[Math.floor(Math.random() * 8)]}'>
+
+          </div>
+        </div>
+      {/each}
+    </div>
+  </div>
+
+<style>
+  @import '/styles.css';
+
+  .holder {
+    width: 80vw;
+    height: 100vh;
+    margin: 0 auto;
+  }
+
+  .holder > #filters {
+    width: 100%;
+    height: 10%;
+  }
+
+  .holder > #content {
+    width: 100%;
+    height: 150vh;
+    display: grid;
+    grid-template-columns: auto auto auto auto;
+    gap: 0px 50px;
+  }
+
+  .holder > #content > .job {
+    height: 300px;
+    background: black;
+  }
+
+  .holder > #content > .job > .colourHolder {
+    height: 50px;
+    width: 100%;
+  }
+
+
+</style>
